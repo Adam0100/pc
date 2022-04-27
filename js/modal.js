3dcc4756fbe8161041cbeb48b58470fe84952d77
Aron Dosybekov <arondosybekov93@gmail.com>
2022-04-27 21:18:14 +0600

    main

---
+++ js/modal.js
@@ -0,0 +1,47 @@
+(function () {
+    const modalBtn = document.querySelector('.modal__button');
+    const modal = document.querySelector('.modal');
+    const courseBtn = document.querySelector('.course__button');
+
+    // Появление модального окна по кнопке "Оставить заявку"
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    })
+
+    // Появление модального окна по кнопке "Заказать курс"
+    courseBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    })
+
+    // // Закрытие модального окна по клику вне зоны модального окна
+    // modal.addEventListener('click', (event) => {
+    //     const modalContent = event.target.closest('.modal__inner');
+
+    //     if (!modalContent) {
+    //         modal.style.display = '';
+    //     }
+    // })
+
+    // Создание "Х" для закрытия модального окна
+    let xBtn = document.createElement('div');
+    // xBtn.className = 'xclose';  не используется
+    xBtn.innerText = 'X';
+
+    modalInner.prepend(xBtn);
+
+    xBtn.style.cssText = `
+        font-size:25px; 
+        position:relative; 
+        display:inline; 
+        padding:10px; 
+        bottom:82px; 
+        left:281px; 
+        cursor:pointer;
+        
+    `;
+
+    // Закрытие модального окна по клику "Х"
+    xBtn.addEventListener('click', () => {
+        modal.style.display = '';
+    })
+})();
